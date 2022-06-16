f63fc4ca2d54d4dac85366a3b707494480c01d04
Jesper Kristensen <cableman@linuxdev.dk>
2022-06-16 15:48:42 +0200

    CMN-75: Filter output to only used paths

--- convert.php
+++ convert.php
@@ -26,19 +26,72 @@ $filterPaths = [
     '/external/{agencyid}/catalog/availability/v3',
     '/external/{agencyid}/catalog/holdings/v3',
 ];
+
 $output = [
     'swagger' => $yaml['swagger'],
     'info' => [
         'title' => 'FBS Adapter',
         'version' => $yaml['info']['version']
     ],
-    'basePath' => '/'
+    'basePath' => '/',
+    'paths' => [],
 ];
 
+$tagsFilter = [];
+$refFilter = [];
 foreach ($yaml['paths'] as $path => $data) {
     if (in_array($path, $filterPaths)) {
-        echo $path,"\n";
+        $output['paths'][$path] = $data;
+        foreach ($data as $op) {
+            $tagsFilter[] = $op['tags'][0];
+            foreach ($op['parameters'] as $parameter) {
+                if (isset($parameter['schema']['$ref'])) {
+                    $parts = explode('/', $parameter['schema']['$ref']);
+                    $refFilter[] = end($parts);
+                }
+            }
+
+            foreach ($op['responses'] as $respons) {
+                if (isset($respons['schema'])) {
+                    if (isset($respons['schema']['$ref'])) {
+                        $parts = explode('/', $respons['schema']['$ref']);
+                        $refFilter[] = end($parts);
+                    } elseif (isset($respons['schema']['items']['$ref'])) {
+                        $parts = explode('/', $respons['schema']['items']['$ref']);
+                        $refFilter[] = end($parts);
+                    }
+                }
+            }
+        }
     }
 }
 
+// Find definitions that $ref other definitions.
+$tmp = array_walk($yaml['definitions'], function ($definition) use (&$refFilter) {
+    foreach ($definition['properties'] as $property) {
+        if (isset($property['$ref'])) {
+            $parts = explode('/', $property['$ref']);
+            $refFilter[] = end($parts);
+        }
+        if (isset($property['items']['$ref'])) {
+            $parts = explode('/', $property['items']['$ref']);
+            $refFilter[] = end($parts);
+        }
+    }
+});
 
+// Only output tags used by the paths.
+$tagsFilter = array_unique($tagsFilter);
+$tags = array_filter($yaml['tags'], function ($tag) use ($tagsFilter) {
+    return in_array($tag['name'], $tagsFilter);
+});
+$output['tags'] = array_values($tags);
+
+// Only output definitions used via $refs.
+$refFilter = array_unique($refFilter);
+$output['definitions'] = array_filter($yaml['definitions'], function ($name) use ($refFilter) {
+    return in_array($name, $refFilter);
+}, ARRAY_FILTER_USE_KEY);
+
+$yaml = Yaml::dump($output, 12);
+file_put_contents($argv[2], $yaml);
